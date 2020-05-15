9a2d91b02f392d8757de0a2bc7f2704341258a2b
Sai Pathuri <sai@pathuri.net>
2020-05-15 15:28:16 -0500

    Auto retry client/token on exception

--- spotify_client.py
+++ spotify_client.py
@@ -1,6 +1,7 @@
 import spotipy
 import settings
 import os
+from spotipy.exceptions import SpotifyException
 
 username = 'saipathuri'
 scopes = 'user-read-currently-playing user-read-playback-state user-modify-playback-state'
@@ -13,31 +14,27 @@ def setup():
     sp = spotipy.Spotify(auth=token)
 
 def get_currently_playing():
-    if token and sp:
-        return sp.current_playback()
-    else:
-        raise Exception("Use setup() first")
+    return check_setup(lambda: sp.current_playback())
 
 def next():
-    if token and sp:
-        return sp.next_track()
-    else:
-        raise Exception("Use setup() first")
+    return check_setup(lambda: sp.next_track())
 
 def previous():
-    if token and sp:
-        return sp.previous_track()
-    else:
-        raise Exception("Use setup() first")
+    return check_setup(lambda: sp.previous_track())
 
 def pause():
-    if token and sp:
-        return sp.pause_playback()
-    else:
-        raise Exception("Use setup() first")
+    return check_setup(lambda: sp.pause_playback())
     
 def play():
+    return check_setup(lambda: sp.start_playback())
+
+def check_setup(func):
     if token and sp:
-        return sp.start_playback()
+        try:
+            return func()
+        except SpotifyException:
+            print("Spotify Exception, rerunning setup")
+            setup()
+            return func()
     else:
         raise Exception("Use setup() first")
